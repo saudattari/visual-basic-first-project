219c303734bb70b83073382b01e211d0c735e7f1
Mohammad Saud <117003607+saudattari@users.noreply.github.com>
2023-05-07 13:42:55 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.PlusButton = new System.Windows.Forms.Button();
+            this.MinusButton = new System.Windows.Forms.Button();
+            this.MultiButton = new System.Windows.Forms.Button();
+            this.DivideButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.warning = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Warning1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(175, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Basic Calculator";
+            // 
+            // PlusButton
+            // 
+            this.PlusButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.PlusButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlusButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.PlusButton.Location = new System.Drawing.Point(48, 242);
+            this.PlusButton.Name = "PlusButton";
+            this.PlusButton.Size = new System.Drawing.Size(93, 32);
+            this.PlusButton.TabIndex = 1;
+            this.PlusButton.Text = "+ Plus";
+            this.PlusButton.UseVisualStyleBackColor = false;
+            this.PlusButton.Click += new System.EventHandler(this.PlusButton_Click);
+            // 
+            // MinusButton
+            // 
+            this.MinusButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.MinusButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinusButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.MinusButton.Location = new System.Drawing.Point(152, 242);
+            this.MinusButton.Name = "MinusButton";
+            this.MinusButton.Size = new System.Drawing.Size(93, 32);
+            this.MinusButton.TabIndex = 2;
+            this.MinusButton.Text = "- Minus";
+            this.MinusButton.UseVisualStyleBackColor = false;
+            this.MinusButton.Click += new System.EventHandler(this.MinusButton_Click);
+            // 
+            // MultiButton
+            // 
+            this.MultiButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.MultiButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MultiButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.MultiButton.Location = new System.Drawing.Point(252, 242);
+            this.MultiButton.Name = "MultiButton";
+            this.MultiButton.Size = new System.Drawing.Size(93, 32);
+            this.MultiButton.TabIndex = 3;
+            this.MultiButton.Text = "* Multi";
+            this.MultiButton.UseVisualStyleBackColor = false;
+            this.MultiButton.Click += new System.EventHandler(this.MultiButton_Click);
+            // 
+            // DivideButton
+            // 
+            this.DivideButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.DivideButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DivideButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.DivideButton.Location = new System.Drawing.Point(351, 242);
+            this.DivideButton.Name = "DivideButton";
+            this.DivideButton.Size = new System.Drawing.Size(93, 32);
+            this.DivideButton.TabIndex = 4;
+            this.DivideButton.Text = "/ Divide";
+            this.DivideButton.UseVisualStyleBackColor = false;
+            this.DivideButton.Click += new System.EventHandler(this.DivideButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(208, 88);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(205, 29);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(208, 156);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(205, 29);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(132, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Value1";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(132, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Value1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(132, 309);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Result";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(208, 304);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(205, 29);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ResetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResetButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ResetButton.Location = new System.Drawing.Point(452, 242);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(93, 32);
+            this.ResetButton.TabIndex = 11;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = false;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // warning
+            // 
+            this.warning.AutoSize = true;
+            this.warning.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.warning.ForeColor = System.Drawing.Color.Red;
+            this.warning.Location = new System.Drawing.Point(337, 209);
+            this.warning.Name = "warning";
+            this.warning.Size = new System.Drawing.Size(0, 13);
+            this.warning.TabIndex = 12;
+            this.warning.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(278, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Warning: ";
+            // 
+            // Warning1
+            // 
+            this.Warning1.AutoSize = true;
+            this.Warning1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Warning1.ForeColor = System.Drawing.Color.Red;
+            this.Warning1.Location = new System.Drawing.Point(337, 209);
+            this.Warning1.Name = "Warning1";
+            this.Warning1.Size = new System.Drawing.Size(12, 13);
+            this.Warning1.TabIndex = 14;
+            this.Warning1.Text = "/";
+            this.Warning1.Click += new System.EventHandler(this.Warning1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(595, 363);
+            this.Controls.Add(this.Warning1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.warning);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.DivideButton);
+            this.Controls.Add(this.MultiButton);
+            this.Controls.Add(this.MinusButton);
+            this.Controls.Add(this.PlusButton);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "                                                                Calculator By Sau" +
+    "d";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button PlusButton;
+        private System.Windows.Forms.Button MinusButton;
+        private System.Windows.Forms.Button MultiButton;
+        private System.Windows.Forms.Button DivideButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.Label warning;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label Warning1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PlusButton_Click(object sender, EventArgs e)
+        {
+            int val1, val2, val3;
+            val1 = Convert.ToInt32(textBox1.Text);
+
+            val2 = Convert.ToInt32(textBox2.Text);
+            if (val1 < -1 && val2 < -1)
+            {
+                string warn1 = "Your Value1 and Value2 is wrong! ";
+                Warning1.Text = warn1;
+            }
+            else if (val1 < -1)
+            {
+                string warn2 = "Your Value1 is wrong!";
+                Warning1.Text = warn2;
+                //Console.ReadLine();
+                //goto b;
+            }
+            else if (val2 < -1)
+            {
+                string warn = "Your Value2 is wrong!";
+                Warning1.Text = warn;
+                //Console.ReadLine();
+                //goto b;
+            }
+            else
+            {
+                Warning1.ResetText();
+                val3 = val1 + val2;
+                textBox3.Text = Convert.ToString(val3);
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+           textBox1.ResetText();textBox2.ResetText();textBox3.ResetText();
+            Warning1.ResetText();
+        }
+
+        private void MinusButton_Click(object sender, EventArgs e)
+        {
+           
+            int val1, val2, val3;
+            val1= Convert.ToInt32(textBox1.Text);
+            
+            val2 = Convert.ToInt32(textBox2.Text);
+            if (val1 < -1 && val2 < -1)
+            {
+                string warn1 = "Your Value1 and Value2 is wrong! ";
+                Warning1.Text = warn1;
+            }
+            else if (val1 < -1)
+            {
+                string warn2 = "Your Value1 is wrong!";
+                Warning1.Text = warn2;
+                //Console.ReadLine();
+                //goto b;
+            }
+            else if (val2 < -1)
+            {
+                string warn = "Your Value2 is wrong!";
+                Warning1.Text = warn;
+                //Console.ReadLine();
+                //goto b;
+            }
+            else
+            {
+                Warning1.ResetText();
+                val3 = val1 - val2;
+                textBox3.Text = Convert.ToString(val3);
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Warning1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MultiButton_Click(object sender, EventArgs e)
+        {
+            int val1, val2,val3;
+            val1 = Convert.ToInt32(textBox1.Text);
+            val2 = Convert.ToInt32(textBox2.Text);
+            if(val1 < -1 && val2 < -1)
+            {
+                string warn1 = "Your Value1 and Value2 is wrong! ";
+                Warning1.Text = warn1;
+            }
+            else if (val1 < -1)
+            {
+
+                string warn2 = "Your Value1 is wrong! ";
+                Warning1.Text = warn2;
+            }
+            else if(val2 < -1)
+            {
+                string warn = "Your Value2 is wrong! ";
+                Warning1.Text = warn;
+            }
+            else
+            {
+                Warning1.ResetText();
+                val3 = val1 * val2;
+                textBox3.Text = Convert.ToString(val3);
+            }
+        }
+
+        private void DivideButton_Click(object sender, EventArgs e)
+        {
+            int val1, val2, val3;
+            val1 = Convert.ToInt32(textBox1.Text);
+            val2 = Convert.ToInt32(textBox2.Text);
+            if (val1 < -1 && val2 < -1)
+            {
+                string warn1 = "Your Value1 and Value2 is wrong! ";
+                Warning1.Text = warn1;
+            }
+            else if (val1 < -1)
+            {
+
+                string warn2 = "Your Value1 is wrong! ";
+                Warning1.Text = warn2;
+            }
+            else if (val2 < -1)
+            {
+                string warn = "Your Value2 is wrong! ";
+                Warning1.Text = warn;
+            }
+            else
+            {
+                Warning1.ResetText();
+                val3 = val1 / val2;
+                textBox3.Text = Convert.ToString(val3);
+            }
+        }
+    }
+}
